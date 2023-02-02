59afbf515cbe55d4f3ed5960a847d0bb84029390
zhangwenda <daerwendeshu@outlook.com>
2023-02-02 23:43:20 +0800

    feat: support PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "SC-VIEWER",
+        short_name: "SC-VIEWER",
+        start_url: ".",
+        description: "A frontend for SCTranslationData",
+        icons: [
+          {
+            "src": "/icon/プロデューサー.webp",
+            "sizes": "180x180",
+            "type": "image/webp",
+          }
+        ]
+      }
+    })
+  ],
+  server: {
+    host: '0.0.0.0'
+  }
 })
