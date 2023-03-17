9e31e593d411886146d53e834f0814600d821917
darwintree <daerwendeshu@outlook.com>
2023-03-17 22:57:14 +0800

    chore: update dependency

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,36 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), 
-    VitePWA({ 
+    vue(),
+    VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        // enabled: true
       },
       manifest: {
-        name: "SC-VIEWER",
-        short_name: "SC-VIEWER",
-        start_url: ".",
-        description: "A frontend for SCTranslationData",
+        name: 'SC-VIEWER',
+        short_name: 'SC-VIEWER',
+        start_url: '.',
+        description: 'A frontend for SCTranslationData',
         icons: [
           {
-            "src": "/icon/プロデューサー.webp",
-            "sizes": "180x180",
-            "type": "image/webp",
-          }
-        ]
-      }
-    })
+            src: '/icon/プロデューサー.webp',
+            sizes: '180x180',
+            type: 'image/webp',
+          },
+        ],
+      },
+    }),
   ],
   server: {
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: [
+      {
+        find: 'vue-i18n',
+        replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
+      },
+    ],
+  },
 })
